a62f121513792a2d7823514aed860bade6dd2752
robghys <rghyselinck@gmail.com>
2021-06-22 20:57:45 +0200

    Implémentation d'une page "not implemented yet"

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -22,4 +22,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll()); // Get a Set of Owners
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
+    }
 }
