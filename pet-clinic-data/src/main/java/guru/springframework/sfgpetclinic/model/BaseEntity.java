627c64f427c95d7d6931abc13cb773eb3c956633
robghys <rghyselinck@gmail.com>
2021-06-26 11:36:55 +0200

    Converted BaseEntity to a JPA-based annotation

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass // The superclass is not associated with any database table.
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //we use boxed type Long instead of long
 
     public Long getId() {
