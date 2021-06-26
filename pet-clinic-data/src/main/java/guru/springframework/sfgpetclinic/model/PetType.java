6c35bb1e7ac7187e5e48217371b34714e893d919
robghys <rghyselinck@gmail.com>
2021-06-26 12:02:43 +0200

    Created JPA Entities for Owner, Pet, and PetType.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,8 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 
+    /**
+     * Getters and Setters
+     */
     public String getName() {
         return name;
     }
