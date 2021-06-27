c0970e03dcd64ef6f101a908272304c068da0d52
robghys <rghyselinck@gmail.com>
2021-06-27 10:17:42 +0200

    Refractored SpecialtyRepository

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgpetclinic.repositories;
 
+import guru.springframework.sfgpetclinic.model.Specialty;
 import org.springframework.data.repository.CrudRepository;
 
 // Will be automatically available in the Spring context as it is a JPA entity
-public interface Specialty extends CrudRepository<Specialty, Long> {
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
 }
